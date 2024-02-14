4560adf446900254adcd328ee3ef2fc834c59a08
DanleiCruz <danlei_brasil@hotmail.com>
2024-02-14 18:10:47 -0300

    feat: meu primeiro commit

---
+++ HelloWorld/src/main/java/danete/danone/megumin/kazuma/Aula02TiposPrimitivos.java
@@ -0,0 +1,17 @@
+package danete.danone.megumin.kazuma;
+
+public class Aula02TiposPrimitivos {
+    public static void  main(String[] args){
+        int age = 35; // numero médio
+        long numeroGrande = 1000000; //numero grande
+        double salarioDouble = 2000.5; //numero grande decimal
+        float salarioFloat = 2500; //numero grande inteiro
+        byte idadeByte = 127; // numero pequeno limitado -128 a 127
+        short idadeShort = 3200; //numero um pouco maior limitado a 33000
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+
+        System.out.println("sexo "+caractere);
+    }
+}
---
+++ HelloWorld/src/main/java/danete/danone/megumin/kazuma/TiposPrimitivosExercicio.java
@@ -0,0 +1,19 @@
+package danete.danone.megumin.kazuma;
+
+public class TiposPrimitivosExercicio {
+    public static void main (String[] args){
+/*
+Crie variaveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+Eu <nome>, morando no <endereço>, confirmo que recebi o salário de <salário>, na data <data>
+ */
+        String nome = "Danete";
+        String endereco = "Rua Prefeito Celso, 619";
+        double salario = 2500.51;
+        String data = "12/02/2024";
+        String relatorio = "Eu "+nome+", morando no endereço "+endereco+", confirmo que recebi o salário de "+salario+
+                ", na data "+data;
+
+        System.out.println(relatorio);
+    }
+}
